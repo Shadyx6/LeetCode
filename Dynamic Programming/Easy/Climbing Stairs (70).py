26c2230a84fc0fcba4a3c58d605a77bc593551d9
Shad <140224513+Shadyx6@users.noreply.github.com>
2026-08-01 12:50:09 +0000

    Climbing Stairs (70).py

---
+++ Dynamic Programming/Easy/Climbing Stairs (70).py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int, memo=None) -> int:
+        if memo is None:
+            memo = {}
+        if n in memo:
+            return memo[n]
+        if n == 1 or n ==2:
+            return n
+        memo[n]= self.climbStairs(n-1, memo) + self.climbStairs(n-2, memo)
+        return memo[n]
