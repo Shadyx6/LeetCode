dfce6e6401ef9d316996d1a8a83e2fc26a9c72a9
Shad <140224513+Shadyx6@users.noreply.github.com>
2026-08-10 13:22:34 +0000

    Move Zeroes @283

---
+++ Two Pointers/Easy/Move Zeroes #283.py
@@ -0,0 +1,13 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        left = 0
+        for right in range(0, n):
+            if nums[right]!= 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left+=1
+            
+        
